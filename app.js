7eacbd78795a6142ce2ab661bac04846d4594186
zma8 <zainabmoosa880@gmail.com>
2025-12-13 14:32:28 +0300

    done

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from public directory
+// Serve root directory as static (for images, etc)
+app.use(express.static(__dirname));
+
+// Serve public directory as static
 app.use(express.static('public'));
 
 // Root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start server
